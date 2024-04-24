3e115e424d56af677c1f5df212477814a0ffca2a
aliciablomqvist <122750659+aliciablomqvist@users.noreply.github.com>
2024-04-24 11:44:20 +0200

    Some Updates

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const button = document.querySelector('.button');
+document.addEventListener("DOMContentLoaded", function () {
+  const button = document.querySelector(".button");
+  const elements = document.querySelectorAll(".title, .text");
 
-    button.addEventListener('click', function () {
-        createHeart();
+  button.addEventListener("click", function () {
+    createHeart();
+    animateText();
+  });
+
+  function createHeart() {
+    const heart = document.createElement("img");
+    heart.src = "images/lovehearts.png";
+    heart.classList.add("heart");
+    heart.style.left = Math.random() * 100 + "vw";
+    heart.style.animationDuration = Math.random() * 2 + 3 + "s";
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+      heart.remove();
+    }, 5000);
+  }
+
+  function animateText() {
+    elements.forEach((el) => {
+      el.classList.add("animate");
+
+      setTimeout(() => {
+        el.classList.remove("animate");
+      }, 4000);
     });
-
-    function createHeart() {
-        const heart = document.createElement('img');
-        heart.src = 'images/lovehearts.png';
-        heart.classList.add('heart');
-        heart.style.left = Math.random() * 100 + 'vw';
-        heart.style.animationDuration = Math.random() * 2 + 3 + 's';
-        document.body.appendChild(heart);
-
-        setTimeout(() => {
-            heart.remove();
-        }, 5000);
-    }
+  }
 });
